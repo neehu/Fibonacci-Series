efa1e2d4db84ceb0a048c804311723e4981dd4f3
CORP\Neeharika.Pathuri <neehu395@gmail.com>
2017-01-21 20:18:28 +0530

    no message

--- Program.cs
+++ Program.cs
@@ -8,24 +8,24 @@ namespace ConsoleApplication4
 {
     class Program
     {
-        
+
         int fibonacci(int number)
         {
             int sum;
-            if (number<=1)
+            if (number <= 1)
             {
                 return 1;
             }
             else
-            sum= (fibonacci(number-1)+fibonacci(number-2));
+                sum = (fibonacci(number - 1) + fibonacci(number - 2));
             return sum;
         }
         static void Main(string[] args)
         {
-            int intial = 100,number;
-            Program calc= new Program();
-            
-            number =calc.fibonacci(intial);
+            int intial = 100, number;
+            Program calc = new Program();
+
+            number = calc.fibonacci(intial);
             Console.WriteLine(Convert.ToString(number));
             Console.ReadKey();
         }
